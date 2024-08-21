a049559763c5c5ef97893e36611f6e9573c4bfe5
user-steven <steven.chow@live.ca>
2024-08-21 15:58:20 -0700

    fix key issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,9 +27,8 @@ const Footer = () => {
       </div>
       <div className="flex items-center justify-center md:gap-3 gap-6 pt-20">
         {socialMedia.map((profile) => (
-          <a href={profile.url} target="_blank" rel="noopener noreferrer">
+          <a key={profile.id} href={profile.url} target="_blank" rel="noopener noreferrer">
             <div
-              key={profile.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-100"
             >
               <img src={profile.img} width={20} height={20} />
